f3f62ceb91aec2229ab7fee7eb28a8c4cd6666a1
Yurii Chuhaienko <chugaenko@gmail.com>
2018-11-01 21:39:48 +0200

    Base version

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # mif
-Node.js microservice framework
+Node.js microservices framework
+
+- Start
+- Load default config
+- Load user config
+- Create current config by merging default and user configs
+- Store configs in app.config
+- Load modules from configs with isActive: true
---
+++ modules/logger/logger.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const winston      = require('winston');
+const moment       = require('moment');
+const {BaseModule} = require('../../index');
+
+/**
+ * Logger module
+ * It exposes `logger` to `this`
+ */
+class Logger extends BaseModule {
+	constructor (app, config) {
+		super(app, config);
+	}
+
+	async init () {
+		winston.addColors({
+			emerg:  'bold yellow bgRed',
+			alert:  'yellow bgRed',
+			crit:   'red bgYellow',
+			error:  'red',
+			warn:   'yellow',
+			notice: 'green',
+			info:   'blue',
+			debug:  'cyan'
+		});
+		const loggerFormat = winston.format.printf((data) => {
+			return `${moment().format(this.config.timeFormat)} ${data.level}: ${data.message}`;
+		});
+		this.app.logger = winston.createLogger({
+			level:  (process.env.NODE_ENV === 'production' ? 'info' : 'debug'),
+			levels: winston.config.syslog.levels,
+			format: winston.format.combine(
+				winston.format.colorize(),
+				loggerFormat
+			),
+			transports: [
+				new winston.transports.Console()
+			]
+		});
+
+		this.app.logger.info('Init module: logger');
+	}
+
+	async start () {
+		// Do nothing
+	}
+
+	async stop () {
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				resolve();
+			}, 5000);
+		});
+	}
+}
+
+module.exports = Logger;
